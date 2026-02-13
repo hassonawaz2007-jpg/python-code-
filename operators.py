e9b72a08fae3f64ef323154904ac5137e5a7535f
hassan nawaz <hassonawaz2007@gmail.com>
2026-02-13 18:32:08 +0500

    learned Oprators in Python

---
+++ operators.py
@@ -0,0 +1,36 @@
+# Arithmetic Operators
+a=23
+b=34
+c=a+b
+print(c)
+
+#Assignment Operators
+a=4-5 #assign 4-2 in a
+print(a)
+b=4
+# b+= 6 #increment the value of b by 6 and then assign it to b
+b-= 6 #decrement the value of b by 6 and then assign it to b
+print(b)
+
+#Comparison Operators
+d=5==5
+print(d)
+
+#logical Operators
+e= True or False
+print(e)
+
+#truth table of 'or'
+print("true or false is", True or False)
+print("true or true is", True or True)
+print("false or true is", False or True)
+print("false or false is", False or False)
+
+#truth table of 'and'
+print("true and false is", True and False)
+print("true and true is", True and True)
+print("false and true is", False and True)
+print("false and false is", False and False)
+
+# not
+print(not(True))
