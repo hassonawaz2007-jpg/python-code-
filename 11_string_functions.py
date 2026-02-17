d4319c377392ce115188ef26e7cdf243c1bb04e4
hassan nawaz <hassonawaz2007@gmail.com>
2026-02-17 16:39:48 +0500

    learned string functions

---
+++ 11_string_functions.py
@@ -0,0 +1,10 @@
+name="hassan"
+
+print(len(name))              #string len
+print(name.endswith("san"))   #string endswith
+print(name.startswith("ha"))  #string startswith  
+print(name.capitalize())      #string capitalize
+print(name.replace("h","p"))  #string replace
+print(name.lower())           #string lower
+print(name.upper())           #string upper
+print(name.split())           #string split
