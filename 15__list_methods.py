2102deee899bd3e7034389296c6b30259e5151aa
hassan nawaz <hassonawaz2007@gmail.com>
2026-02-21 01:49:56 +0500

    learned lists methods

---
+++ 15__list_methods.py
@@ -0,0 +1,13 @@
+friends=["apple", "orange", 5,345.5 , "hassan", "musab"]
+print(friends)
+
+friends.append("hussain")
+print(friends)
+
+l1=[1,9,0,2,3,95,36,67]
+# l1.sort()
+# l1.reverse()
+#l1.insert(4,654) #insert 654 such its index in the list is 3
+# print(l1.pop(3))
+l1.remove(95)
+print(l1) 
