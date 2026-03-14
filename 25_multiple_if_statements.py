d95f899b546af36658e1a9c3eca002d26144bb17
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-15 00:35:11 +0500

    learned about multiple if statements

---
+++ 25_multiple_if_statements.py
@@ -0,0 +1,23 @@
+a=int(input("Enter your age:"))
+
+# if statement no:1
+if(a%2 ==0):
+    print("a is even")
+    
+# end of if statement no:1    
+    
+# if statement no:2
+if(a>=18) :
+    print("you are above the age of consent")
+    print("good fo5 you")
+
+elif(a<0):
+    print("you are entering an invalid negative age")  
+
+elif(a==0):
+    print("you are entering 0 is not a valid age")  
+     
+else:    
+    print("you are below the age of consent")
+# end of if statement no:1
+print("end of porogram")
