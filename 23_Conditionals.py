5c4ac1e02ca7d419bf5c278355f677998c8c5544
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-14 23:58:30 +0500

    learned about conditionals in python

---
+++ 23_Conditionals.py
@@ -0,0 +1,21 @@
+a=int(input("Enter your age:"))
+
+#if elif else ladder
+if(a>=18) :
+    print("you are above the age of consent")
+    print("good fo5 you")
+    
+
+elif(a<0):
+    print("you are entering an invalid negative age")  
+
+
+elif(a==0):
+    print("you are entering 0 is not a valid age")  
+     
+  
+      
+else:    
+    print("you are below the age of consent")
+
+print("end of porogram")
