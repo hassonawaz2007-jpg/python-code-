a379d1c9cde8575eb5fa68381295548cbfe28087
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-27 15:48:59 +0500

    learned about break and continue statement

---
+++ 32_break_and_continue.py
@@ -0,0 +1,12 @@
+#BREAK STATEMENT
+for i in range(200):
+    if(i==34):
+        break #Exit the loop right now;
+    print(i)
+    
+    
+#CONTINUE STATEMENT
+for i in range(200):
+    if(i==34):
+        continue #Skip the iteration mean skip the mentioned number;
+    print(i)
