35a006f1acef87673d4d54dd0a05d9a0fe2bb3b5
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-27 15:21:36 +0500

    learned about for loop eith list,tuples and strings

---
+++ 30_for_loop_iterate.py
@@ -0,0 +1,16 @@
+#for loops with list
+l=[1,23,34,54,657]
+for i in l:
+    print(i)
+        
+#for ;oops with tuples
+t=(3,4,67,87)
+for i in t:
+    print(i)
+    
+#for loops with strings
+s="hassan"
+for i in s:
+    print(i)
+    
+    
