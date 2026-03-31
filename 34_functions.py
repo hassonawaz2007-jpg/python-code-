6e171bd893226619f687ff68f8f00803f036f1f2
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-31 12:21:27 +0500

    learned about functions in python and its types

---
+++ 34_functions.py
@@ -0,0 +1,31 @@
+#a=int(input("enter your number: "))
+#b=int(input("enter your number: "))
+#c=int(input("enter your number: "))
+
+#average=(a+b+c)/3
+#print(average)
+
+
+#a=int(input("enter your number: "))
+#b=int(input("enter your number: "))
+#c=int(input("enter your number: "))
+#average=(a+b+c)/3
+#print(average)
+
+#Function Definition
+def avg():
+    a=int(input("enter a number: "))
+    b=int(input("enter a number: "))
+    c=int(input("enter a number: "))
+    
+    average=(a+b+c)/3
+    print(average)
+    
+avg()#Function call
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+print("Thank you!")
+avg()
+avg()
---
+++ 35_quick_quiz.py
@@ -0,0 +1,4 @@
+def func1():
+    print("good day")
+    
+func1()
