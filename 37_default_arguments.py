05a08fef97b4567029e80ef42f0566414d821cfb
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-31 13:25:19 +0500

    learned about the defaults  in the arguments

---
+++ 37_default_arguments.py
@@ -0,0 +1,6 @@
+def func1(name,ending="Thank you"):#in this the thank you is default if we forget to mention ending in the end rhen this will put as default
+    print("good day,"+name)
+    print(ending)
+    
+func1("hassan","thanks")
+func1("musab")
