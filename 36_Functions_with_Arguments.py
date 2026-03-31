7eadf443caec2038e2734d9b0d7f83084661885b
hassan nawaz <hassonawaz2007@gmail.com>
2026-03-31 13:00:23 +0500

    learned about funtions in arguments

---
+++ 36_Functions_with_Arguments.py
@@ -0,0 +1,14 @@
+def func1(name,ending):
+    print("good day,"+name)
+    print(ending)
+    
+func1("hassan", "thank you")
+
+#this one is using return
+'''def func1(name,ending): 
+    print("good day,"+name)
+    print(ending)
+    return "ok"
+    
+a= func1("harry","thank you")
+print(a)'''
