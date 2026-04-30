cc1ab158221857d196c4b9102580b2347b6279dc
hassan nawaz <hassonawaz2007@gmail.com>
2026-04-30 18:47:05 +0500

    learned class in oops

---
+++ 45_class.py
@@ -0,0 +1,14 @@
+class employee:
+    language="py" #this is a class attribute
+    salary=1200000
+    
+    
+hassan=employee()
+hassan.name="hassan" #this is an instance(onbject) aattribute
+print(hassan.name,hassan.language,hassan.salary)
+
+musab=employee()
+musab.name="musab"
+print(musab.name,musab.salary,musab.language)
+
+#here name is instance(object) attribute salary and language are class attributes as they directly belong to the class attributes
