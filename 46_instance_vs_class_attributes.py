d26cedecea6667a93e865ccd3e6f92e25689421d
hassan nawaz <hassonawaz2007@gmail.com>
2026-04-30 18:55:44 +0500

    instance vs class attributes

---
+++ 46_instance_vs_class_attributes.py
@@ -0,0 +1,8 @@
+class employee:
+    language="py" #this is a class attribute
+    salary=1200000
+    
+    
+hassan=employee()
+hassan.language="javascript" #this is an instance(onbject) aattribute
+print(hassan.language,hassan.salary)
