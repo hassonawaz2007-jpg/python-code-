1833726a73eafabd75a3d9e11ec0b4c17c0941ec
hassan nawaz <hassonawaz2007@gmail.com>
2026-05-30 14:46:43 +0500

    learned about raising exceptions

---
+++ 61_raising_exceptions.py
@@ -0,0 +1,6 @@
+a=int (input("enter a number :"))
+b=int (input("enter second number :"))
+if(b==0):
+    raise ZeroDivisionError("hey our porogram is not meant to divide numbers by zero")
+else:
+    print(f"the division a/b is{a/b}")
